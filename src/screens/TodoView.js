29671a777d22d1e39e1eaade6a81d069535e170e
Christopher Fields <chrisjr06@gmail.com>
2020-02-07 15:11:40 -0600

    updated TodoView to use toggleTodo function from model

--- src/screens/TodoView.js
+++ src/screens/TodoView.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 
-import {ViewModelContext} from '../contexts';
+import { ViewModelContext } from '../contexts';
 
 @observer
 class TodoView extends React.Component {
@@ -14,11 +14,19 @@ class TodoView extends React.Component {
       <div style={{ padding: 20 }}>
         <h1>React & MobX Todos</h1>
         <div style={{ margin: 10 }}>
-          <button style={{ margin: 10 }} onClick={() => model.add()}>New Todo</button>
-          <button style={{ margin: 10 }} onClick={() => model.load()}>Reload Todos</button>
-          <button style={{ margin: 10 }} onClick={() => model.save()}>Save Todos</button>
+          <button style={{ margin: 10 }} onClick={() => model.add()}>
+            New Todo
+          </button>
+          <button style={{ margin: 10 }} onClick={() => model.load()}>
+            Reload Todos
+          </button>
+          <button style={{ margin: 10 }} onClick={() => model.save()}>
+            Save Todos
+          </button>
         </div>
-        {model.todos.map((todo, index) => <TodoDetail key={todo.id} model={model} todo={todo} index={index} />)}
+        {model.todos.map((todo, index) => (
+          <TodoDetail key={todo.id} model={model} todo={todo} index={index} />
+        ))}
         <br />
         <TodosMeta />
       </div>
@@ -35,9 +43,22 @@ class TodoDetail extends React.Component {
       <div>
         <strong style={{ color: todo.isDone ? 'green' : 'red' }}>#{index + 1} |</strong>
 
-        <input style={{ margin: 5 }} type="checkbox" checked={todo.isDone} onChange={e => todo.isDone = e.target.checked} />
-        <input size={200} style={{ margin: 5, padding: 5 }} type="text" value={todo.text} onChange={e => todo.text = e.target.value} />
-        <button style={{ margin: 5 }} onClick={() => model.remove(todo)}>Delete</button>
+        <input
+          style={{ margin: 5 }}
+          type="checkbox"
+          checked={todo.isDone}
+          onChange={e => model.toggleTodo(todo)}
+        />
+        <input
+          size={200}
+          style={{ margin: 5, padding: 5 }}
+          type="text"
+          value={todo.text}
+          onChange={e => (todo.text = e.target.value)}
+        />
+        <button style={{ margin: 5 }} onClick={() => model.remove(todo)}>
+          Delete
+        </button>
       </div>
     );
   }
@@ -46,13 +67,13 @@ class TodoDetail extends React.Component {
 @observer
 class TodosMeta extends React.Component {
   static contextType = ViewModelContext;
-  
+
   render() {
-    const {todoViewModel} = this.context;
+    const { todoViewModel } = this.context;
     const totalTodos = todoViewModel.todos.length;
     const numValidTodos = todoViewModel.todos.filter(todo => todo.isValid).length;
     const numCompleteTodos = todoViewModel.todos.filter(todo => todo.isDone).length;
-  
+
     return (
       <div style={{ border: '1px solid black', padding: 10 }}>
         <h2>Todos Metadata</h2>
